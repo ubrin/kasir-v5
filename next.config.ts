24e782c7d46fab9977080674aa97c4f78f9e6b6d
Ubrin Octari Sandi Priatama <UbrinOctari@gmail.com>
2025-07-17 23:43:23 +0000

    saya sudah me ngupload logo berbagai ukuran cek dulu

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import withPWAInit from 'next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
-  // disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
 });
