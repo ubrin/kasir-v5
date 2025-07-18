d109758deb303394777953124afc3d578a4b9c60
Ubrin Octari Sandi Priatama <UbrinOctari@gmail.com>
2025-07-18 08:08:04 +0000

    tombol download belum ada di browser hp saya bisakah menyiapkan file ber

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
         <meta name="theme-color" content="#3F51B5" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
