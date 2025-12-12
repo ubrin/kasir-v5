91f1b286e1d5ac9dd91a2273e48132ff42a0adff
Ubrin Octari Sandi Priatama <UbrinOctari@gmail.com>
2025-12-12 03:48:50 +0000

    pada invoice bisa kah kita membuat invoice lebih resmi untuk di tujukan

--- src/app/(app)/invoice/[id]/page.tsx
+++ src/app/(app)/invoice/[id]/page.tsx
@@ -93,6 +93,7 @@ export default function InvoicePage() {
           const imgData = canvas.toDataURL('image/png');
           const pdf = new jsPDF('p', 'mm', 'a4');
           const pdfWidth = pdf.internal.pageSize.getWidth();
+          const pdfHeight = pdf.internal.pageSize.getHeight();
           const canvasWidth = canvas.width;
           const canvasHeight = canvas.height;
           const ratio = canvasWidth / canvasHeight;
